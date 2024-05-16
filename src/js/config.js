e1b413a0c1ddd421bcbedd7d12d8c89083097c36
Ericsson Raphael <ericssonraphael@gmail.com>
2024-05-16 17:51:41 +0100

    Modified config file

--- src/js/config.js
+++ src/js/config.js
@@ -1,12 +1,10 @@
-dotenv.config({ path: './config.env' });
-
-export const API_URL = process.env.API_URL;
-export const TIMEOUT_SEC = process.env.TIMEOUT_SEC;
-export const RES_PER_PAGE = process.env.RES_PER_PAGE;
-export const KEY = process.env.KEY;
-export const MODAL_CLOSE_SEC = process.env.MODAL_CLOSE_SEC;
-export const ADD_FIELD_MAX = process.env.ADD_FIELD_MAX;
-export const API_URL_2 = process.env.API_URL_2;
-export const X_APP_ID = process.env.X_APP_ID;
-export const X_APP_KEY = process.env.X_APP_KEY;
-export const X_REMOTE_USER_ID = process.env.X_REMOTE_USER_ID;
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 20;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a74b70e1-6a90-4510-a0ac-564aaee32e84';
+export const MODAL_CLOSE_SEC = 2.5;
+export const ADD_FIELD_MAX = 10;
+export const API_URL_2 = `https://trackapi.nutritionix.com/v2/natural/nutrients`;
+export const X_APP_ID = '671df85c';
+export const X_APP_KEY = '200386756533a25759fa7042ebf7cfb3';
+export const X_REMOTE_USER_ID = '0';
