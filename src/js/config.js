b1dda2212b94802b0e55e8652811506dd3ee2e1b
Ericsson Raphael <ericssonraphael@gmail.com>
2024-05-16 11:13:47 +0100

    Modified config file

---
+++ src/js/config.js
@@ -0,0 +1,12 @@
+dotenv.config({ path: './config.env' });
+
+export const API_URL = process.env.API_URL;
+export const TIMEOUT_SEC = process.env.TIMEOUT_SEC;
+export const RES_PER_PAGE = process.env.RES_PER_PAGE;
+export const KEY = process.env.KEY;
+export const MODAL_CLOSE_SEC = process.env.MODAL_CLOSE_SEC;
+export const ADD_FIELD_MAX = process.env.ADD_FIELD_MAX;
+export const API_URL_2 = process.env.API_URL_2;
+export const X_APP_ID = process.env.X_APP_ID;
+export const X_APP_KEY = process.env.X_APP_KEY;
+export const X_REMOTE_USER_ID = process.env.X_REMOTE_USER_ID;
